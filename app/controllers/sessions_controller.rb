a7e086072dbc9f1d953cbd01b3136d8bdc14066a
Arda Zafer İbin <z.ardaibin@gmail.com>
2019-01-25 15:09:10 +0300

    advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,6 +11,7 @@ class SessionsController < ApplicationController
 
     if user && user.authenticate(password)
       log_in user
+      params[:session][:remember_me]  == '1' ? remember(user) : forget(user)
       redirect_to user
     else
       flash.now[:danger] ='Invalid email/password combination'
@@ -21,7 +22,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_path
   end
 
