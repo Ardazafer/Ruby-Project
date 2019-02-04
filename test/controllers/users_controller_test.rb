45817375e34a2494797bd3354a0bfeaf4924edf3
Arda Zafer İbin <z.ardaibin@gmail.com>
2019-02-04 15:27:59 +0300

    updating users

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,6 +1,14 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+
+    @user       = users(:michael)
+ 
+    @other_user = users(:archer)
+
+  end
   test "should get new" do
     get signup_path
     assert_response :success
